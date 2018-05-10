d3ed31150069fb8260b874e66135316f0716b678
Vivek Kumar Gautam <Kadaslki@users.noreply.github.com>
2018-05-10 15:27:43 +0530

    Add files via upload

---
+++ Curriculum_Vitae/cvmodel.php
@@ -0,0 +1,20 @@
+<?php
+	//File containing the 
+	class acad{	//Defining class for academic qualification
+		var $boarduniv,$year,$percgpa;
+	}
+	
+	class xp{	//Defining class for experience
+		var $insti,$strdate,$stpdate;
+	}
+	
+	class person{	//Defining class for person's attributes
+		var $name,$admnno,$dob,$email;	
+		var $gender,$caddress,$skypeid,$paddress;
+		var $mobno,$category,$nationality;
+		$acdmc[] = new acad;
+		$xp[] = new xp;
+		
+	}
+	
+?>
