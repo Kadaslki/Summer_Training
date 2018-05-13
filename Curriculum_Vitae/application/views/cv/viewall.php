d8af25dd03c3dbcaa0d1b36d8c7cbdaaa24791b2
Vivek Kumar Gautam <Kadaslki@users.noreply.github.com>
2018-05-13 21:46:22 +0530

    Add files via upload

--- Curriculum_Vitae/application/views/cv/viewall.php
+++ Curriculum_Vitae/application/views/cv/viewall.php
@@ -1,4 +1,30 @@
-<?php foreach ($cv as $cv_item): ?>
-	<a href="http://localhost:8888/cv/index.php/cv/view/<?php echo $cv_item['admnno']?>"><?php echo $cv_item['name'] ?></a>
-	</br>
-<?php endforeach ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+	
+	<style>
+	h1{
+		background-color:  #337ab7;
+		color: white;
+		text-align: center
+	}
+	ul{
+		list-style-type: square;
+	}
+	</style>
+
+	<?php foreach ($cv as $cv_item): ?>
+		<ul>
+			<li><a href="http://localhost:8888/cv/index.php/cv/view/<?php echo $cv_item['admnno']?>"><?php echo $cv_item['name'] ?></a>
+		</br>
+		</ul>
+	<?php endforeach ?>
+	</body>
+</html>
