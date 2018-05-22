912d517e2e742a200b484a4c09c14167d57fec69
Vivek Kumar Gautam <Kadaslki@users.noreply.github.com>
2018-05-22 16:06:06 +0530

    Add files via upload

---
+++ Contiki/project-conf.h
@@ -0,0 +1,9 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+#define RF_CORE_CONF_CHANNEL                     20
+
+// #define NETSTACK_CONF_RDC     nullrdc_driver
+
+#define IEEE_ADDR_CONF_ADDRESS {1,2,3,4,5,6,1,15}
+
+#endif /* PROJECT_CONF_H_ */
